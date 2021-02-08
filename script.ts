e654f817a5236079fecd299a8a801f43080c3be7
David Ajayi <davidrums65@gmail.com>
2021-02-08 14:05:38 +0100

    learnt about functions as types

--- script.ts
+++ script.ts
@@ -27,3 +27,10 @@ function sayHello(): void {
     console.log('hello')
 }
 
+function multi(value1: number, value2: number): number {
+    return value1 * value2
+}
+
+// functions as types
+let myFunction: (val1: number, val2: number) => number 
+myFunction = multi
