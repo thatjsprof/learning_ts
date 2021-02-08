d14cb957cfae253dc73dcb2a29ee8fa1687d8fc3
David Ajayi <davidrums65@gmail.com>
2021-02-08 13:55:48 +0100

    learnt about the any type

--- script.ts
+++ script.ts
@@ -1,11 +1,19 @@
+// type: string
 let string: string = 'holla'
+// type: number
 let number: number = 21
+// type: any
 let variable: any = '1'
+// type: array
 let array: any[] = ['holla', 'thanks']
+// type: tuple
 let tuple: [number, string] = [34, 'holla']
+// type: enum
 enum Color {
     Gray,
     Green = 100,
     Blue
 }
-let myColor: Color = Color.Green
+// type: any
+let car: any = 'dkldls'
+car = 213
